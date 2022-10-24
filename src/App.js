5b623b6e86162640419ac39e7e822b7f94d3722d
Thais Rosa <tsrosa18@gmail.com>
2022-10-24 17:13:52 -0300

    Requisitos 16 a 18 - footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RecipesProvider from './context/RecipesProvider';
 import Login from './Pages/Login';
+import Meals from './Pages/Meals';
+import Drinks from './Pages/Drinks';
+import Profile from './Pages/Profile';
+import MealsReceita from './Pages/MealsReceita';
+import DrinksReceita from './Pages/DrinksReceita';
+import MealsInProgress from './Pages/MealsInProgress';
+import DrinksInProgress from './Pages/DrinksInProgress';
+
+import DoneRecipes from './Pages/DoneRecipes';
+import Favourites from './Pages/Favourites';
 
 function App() {
   return (
@@ -12,6 +22,23 @@ function App() {
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={ Login } />
+            <Route exact path="/meals" component={ Meals } />
+            <Route exact path="/drinks" component={ Drinks } />
+            <Route exact path="/meals/:id-da-receita" component={ MealsReceita } />
+            <Route exact path="/drinks/:id-da-receita" component={ DrinksReceita } />
+            <Route
+              exact
+              path="/meals/:id-da-receita/in-progress"
+              component={ MealsInProgress }
+            />
+            <Route
+              exact
+              path="/drinks/:id-da-receita/in-progress"
+              component={ DrinksInProgress }
+            />
+            <Route path="/profile" component={ Profile } />
+            <Route path="/done-recipes" component={ DoneRecipes } exact />
+            <Route path="/favorite-recipes" component={ Favourites } exact />
             {/* <Route path="*" component={ NotFound } /> */}
           </Switch>
         </BrowserRouter>
---
+++ src/Pages/MealsReceita.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function MealsReceita() {
+  return (
+    <div>MealsReceita</div>
+  );
+}
