06e611a8db724ee070c37befdf4e772308208dad
Elivelton <eliveltonmn@outlook.com>
2022-11-01 18:50:28 -0300

    Grupo 21

--- src/Pages/FavoriteRecipes.jsx
+++ src/Pages/FavoriteRecipes.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import copy from 'clipboard-copy';
-
+//
 import Header from '../components/Header';
 
 import blackHeartIcon from '../images/blackHeartIcon.svg';
